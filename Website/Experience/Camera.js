c16740520d9c72094aaa2ca4b09a50feac3c67a6
Nam Luong <namluongminh2509@gmail.com>
2022-11-22 19:49:58 -0800

    Start Renderer.js and Modify Camera.js

--- Website/Experience/Camera.js
+++ Website/Experience/Camera.js
@@ -1,15 +1,53 @@
-import Experience from"./Experience.js"
+import * as THREE from "three"
+import Experience from "./Experience.js"
 
 export default class Camera{
     constructor(){
         this.experience = new Experience();
         this.sizes = this.experience.sizes;
-        this.scene = this.experience.scence;
+        this.scene = this.experience.scene;
         this.canvas = this.experience.canvas;
-        console.log("Hello World")
-        console.log(this.experience)
-        console.log(this.sizes)
-        console.log(this.scence)
-        console.log(this.canvas)
+        
+        this.createPerspectiveCamera();
+        this.createOrthographicCamera();
+
+    }
+
+    createPerspectiveCamera(){
+        this.perspectiveCamera = new THREE.PerspectiveCamera(
+            35,
+            this.sizes.aspect,
+            0.1,
+            1000
+        );
+        this.scene.add(this.perspectiveCamera)
+    }
+
+    createOrthographicCamera(){
+        this.frustrum = 5;
+        this.orthographicCamera = new THREE.OrthographicCamera(
+            (-this.sizes.aspect * this.sizes.frustrum)/2,
+            (this.sizes.aspect * this.sizes.frustrum)/2,
+            this.sizes.frustrum / 2,
+            -this.sizes.frustrum / 2,
+            -100,
+            100
+        );
+        this.scene.add(this.orthographicCamera)
+    }
+
+    resize(){
+        this.perspectiveCamera = this.sizes.aspect;
+        this.perspectiveCamera.updateProjectionMatrix();
+
+        this.orthographicCamera.left = (-this.sizes.aspect * this.sizes.frustrum)/2;
+        this.orthographicCamera.right =  (this.sizes.aspect * this.sizes.frustrum)/2;
+        this.orthographicCamera.top = this.sizes.frustrum / 2;
+        this.orthographicCamera.bottom = this.sizes.frustrum / 2;
+        this.orthographicCamera.updateProjectionMatrix();
+
+    }
+    update(){
+        
     }
 }
