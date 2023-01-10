23b3891b758ab633050d18a31d51f951c30c0200
Nam Luong <namluongminh2509@gmail.com>
2023-01-09 16:46:21 -0800

    3D

---
+++ Website/Experience/World/World.js
@@ -0,0 +1,17 @@
+import * as THREE from "three"
+import Experience from "../Experience"
+
+import Room from "./Room"
+
+export default class World {
+    constructor(){
+        this.experience = new Experience()
+        this.sizes = this.experience.sizes
+        this.scene = this.experience.scene
+        this.canvas = this.experience.canvas
+        this.camera = this.experience.camera
+    }
+    resize(){}
+
+    update(){}
+}
