4dd7df7a4fe17acf8254967d05700978b8c514a0
Nam Luong <namluongminh2509@gmail.com>
2023-01-12 03:27:35 -0800

    Add video to screen and add shadow

--- Website/Experience/World.js/Room.js
+++ Website/Experience/World.js/Room.js
@@ -16,8 +16,19 @@ export default class Room{
         this.actualRoom.children.forEach(child => {
             child.castShadow = true;
             child.receiveShadow =  true;
-
-            console.log(child)
+            console.log(child.name)
+            if(child instanceof THREE.Group){
+                child.children.forEach((groupchild)=>{
+                    groupchild.castShadow = true;
+                    //groupchild.receiveShadow = true;
+                    
+                });
+            }
+            if(child.name === "Screen"){
+                child.material = new THREE.MeshBasicMaterial({
+                    map: this.resources.items.screen,
+                });
+            }
         });
 
         this.scene.add(this.actualRoom);
