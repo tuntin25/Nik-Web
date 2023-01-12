5ae7ba2a88d65ccda3c04af39d7c611f3f876659
Nam Luong <namluongminh2509@gmail.com>
2023-01-12 00:29:47 -0800

    Modify Dimension of room

--- Website/Experience/World.js/Room.js
+++ Website/Experience/World.js/Room.js
@@ -15,7 +15,7 @@ export default class Room{
     setModel(){
         this.scene.add(this.actualRoom);
         this.actualRoom.scale.set(0.11,0.11,0.11);
-        //this.actualRoom.rotation.y = Math.PI;
+        this.actualRoom.rotation.y = Math.PI;
     }
     resize(){
 
