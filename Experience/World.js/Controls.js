2e8b81fa4513674735b116fecdf2860abe7389dd
Nam Luong <namluongminh2509@gmail.com>
2023-01-19 01:23:28 -0800

    Adjust scrolltrigger

--- Experience/World.js/Controls.js
+++ Experience/World.js/Controls.js
@@ -2,11 +2,13 @@ import * as THREE from "three"
 import Experience from "../Experience.js"
 
 import GSAP from "gsap";
-import {ScrollTrigger} from "gsap/ScrollTrigger";
+import {ScrollTrigger} from "gsap/ScrollTrigger.js";
+
 export default class Controls{
     constructor(){
         this.experience = new Experience();
-        this.scene = this.experience.scene
+        this.scene = this.experience.scene;
+        this.sizes = this.experience.sizes;
         this.resources = this.experience.resources;
         this.time = this.experience.time;
         this.camera = this.experience.camera;
@@ -19,14 +21,20 @@ export default class Controls{
     }
 
     setPath(){
-        
+        console.log(this.room)
         this.timeline = new GSAP.timeline();
-        // this.timeline.to(this.room.position,{
-        //     x:2,
-        //     ScrollTrigger:{
-        //         trigger: ".first-move"
-        //     }
-        // });
+        this.timeline.to(this.room.position,{
+            x:()=>{
+                return this.sizes.width * 0.00125},
+            scrollTrigger:{
+                trigger: ".first-move",
+                markers:true,
+                start:"top top",
+                end: "bottom bottom",
+                scrub: 0.6,
+                invalidateOnRefresh: true,
+            }
+        });
         
     }
     resize(){
--- Experience/World.js/World.js
+++ Experience/World.js/World.js
@@ -25,7 +25,7 @@ export default class World {
     }
 
     resize(){
-
+            
     }
 
     update(){
