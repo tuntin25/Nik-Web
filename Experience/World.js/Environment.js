42b2fec3e015a926dae1e63efc9452bbb9c8ebae
Nam Luong <namluongminh2509@gmail.com>
2023-05-16 22:51:57 -0700

    Update Ambient Light

--- Experience/World.js/Environment.js
+++ Experience/World.js/Environment.js
@@ -39,11 +39,11 @@ export default class Environment{
         this.sunLight.position.set(-1.5, 7, 3);
         this.scene.add(this.sunLight)
 
-        this.ambientLight = new THREE.AmbientLight("#ffffff",1)
+        this.ambientLight = new THREE.AmbientLight("#ffffff",3)
         this.scene.add(this.ambientLight)
     }
     switchTheme(theme){
-        console.log(this.sunLight)
+        //console.log(this.sunLight)
         if(theme === "dark"){
             // console.log(theme)
             GSAP.to(this.sunLight.color,{
