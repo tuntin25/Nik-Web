549d61dbc67de7fa5a72d61c6d8ceff22f2ae611
Nam Luong <namluongminh2509@gmail.com>
2023-05-24 12:14:14 -0700

    Shadow Adjustment

--- Experience/World.js/Environment.js
+++ Experience/World.js/Environment.js
@@ -36,7 +36,7 @@ export default class Environment{
         this.sunLight.shadow.normalBias = 0.05;
         // const helper = new THREE.CameraHelper(this.sunLight.shadow.camera);
         // this.scene.add(helper);
-        this.sunLight.position.set(-1.5, 7, 3);
+        this.sunLight.position.set(-1.5, 5,3);
         this.scene.add(this.sunLight)
 
         this.ambientLight = new THREE.AmbientLight("#ffffff",2.5)
