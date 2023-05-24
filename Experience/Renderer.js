50615f0e3847aad09ac7feb7e8f20b67a908750d
Nam Luong <namluongminh2509@gmail.com>
2023-05-23 17:47:54 -0700

    Clean Up

--- Experience/Renderer.js
+++ Experience/Renderer.js
@@ -8,8 +8,7 @@ export default class Renderer{
         this.scene = this.experience.scene;
         this.canvas = this.experience.canvas;
         this.camera = this.experience.camera;
-        //console.log(this.canvas)
-        //console.log(this.camera,this.camera.perspectiveCamera)
+        
         this.setRenderer();
     }
 
