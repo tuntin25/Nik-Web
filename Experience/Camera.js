103a6ff17fa90fcb672b122542252bbec8e0444c
Nam Luong <namluongminh2509@gmail.com>
2023-05-24 13:26:29 -0700

    Fix Resizing issue

--- Experience/Camera.js
+++ Experience/Camera.js
@@ -74,7 +74,7 @@ export default class Camera{
         this.orthographicCamera.left = (-this.sizes.aspect * this.sizes.frustrum)/2;
         this.orthographicCamera.right =  (this.sizes.aspect * this.sizes.frustrum)/2;
         this.orthographicCamera.top = this.sizes.frustrum / 2;
-        this.orthographicCamera.bottom = this.sizes.frustrum / 2;
+        this.orthographicCamera.bottom = -this.sizes.frustrum / 2;
         this.orthographicCamera.updateProjectionMatrix();
     }
     update(){
--- Experience/Utils/Sizes.js
+++ Experience/Utils/Sizes.js
@@ -8,11 +8,11 @@ export default class Sizes extends EventEmitter{
         this.aspect = this.width/this.height;
         this.pixelRatio = Math.min(window.devicePixelRatio,2)
         this.frustrum = 5;
-        // if (this.width < 968) {
-        //     this.device = "mobile";
-        // } else {
-        //     this.device = "desktop";
-        // }
+        if (this.width < 968) {
+            this.device = "mobile";
+        } else {
+            this.device = "desktop";
+        }
 
         window.addEventListener("resize", () => {
             this.width = window.innerWidth;
@@ -21,13 +21,13 @@ export default class Sizes extends EventEmitter{
             this.pixelRatio = Math.min(window.devicePixelRatio,2);
             this.emit("resize");
             
-            // if (this.width < 968 && this.device !== "mobile") {
-            //     this.device = "mobile";
-            //     this.emit("switchdevice", this.device);
-            // } else if (this.width >= 968 && this.device !== "desktop") {
-            //     this.device = "desktop";
-            //     this.emit("switchdevice", this.device);
-            // }
+            if (this.width < 968 && this.device !== "mobile") {
+                this.device = "mobile";
+                this.emit("switchdevice", this.device);
+            } else if (this.width >= 968 && this.device !== "desktop") {
+                this.device = "desktop";
+                this.emit("switchdevice", this.device);
+            }
         });
     }
 }
